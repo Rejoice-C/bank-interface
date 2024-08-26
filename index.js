1760192cd3eb10b0d191ddd51c0304209bdf6fd5
Rejoice <chirejoice90@gmail.com>
2024-08-26 19:26:41 +0100

    added js

---
+++ index.js
@@ -0,0 +1,34 @@
+// const keeper = document.getElementById ('keeper');
+// console.log(keeper.innerText);
+// document.querySelector('.bank').innerText = keeper.innerText;
+function myFunction(){
+    alert ("my bank");
+}
+
+function myFund(){
+    alert("Funds")
+}
+
+function myPension(){
+    alert("Pension")
+}
+
+function myEase(){
+    alert("@Ease")
+}
+
+function myInsurance(){
+    alert("Insurance")
+}
+
+function myStock(){
+    alert("Stock")
+}
+
+function myLifeInsurance(){
+    alert("Life Insurance")
+}
+    
+function myOnePass(){
+    alert("OnePass")
+}
